5cd9e6aabefe7818044e22186a68644fb3f0aaf7
yhammeke <yuriy@hammeke.de>
2024-01-07 22:09:55 +0100

    Bugfixing and code consolidation

--- amg_bobby_car.py
+++ amg_bobby_car.py
@@ -76,72 +76,28 @@ def playEngineStartSound():
         logging.exception("Error occurred while loading mp3 file")
 
 def startEngine():
+    global AMGBobbyCarIgnitionState
     print("Engine Start Process over the Ignition Switch")
     setICLightsToOff()
     playEngineStartSound()
     fadeInTheLights()
+    AMGBobbyCarIgnitionState = 1
 
+# By Transition to Engine Off:
+# Stop all sounds
+# Stop the Music Player
+# Let the lights according to the Light Switch State (to be implemented later)
 def stopEngine():
+    global AMGBobbyCarIgnitionState
     print("Engine Stop Process over the Ignition Switch")
+    stopMusicPlayer()
     setIgnitionToOff()
+    AMGBobbyCarIgnitionState = 0
 
 #####################################
 # END OF ENGINE RELATED FUNCTIONS   #
 #####################################
 
-def startMusicMode():
-    print("Music Mode started")
-    # Control LEDs
-    led_front_left.blink(0,0,1,1)
-    sleep(1)
-    led_front_right.blink(0,0,1,1)
-    sleep(0.5)
-    led_rear.blink(0,0,1,1)
-    
-
-def startRandomSong():
-    # TODO: no song repetitions for at least 5 songs.
-    # Select random song from the list
-    randomSong = random.randrange(0,len(songs))
-    previousSong = randomSong
-    print('Random song number:', randomSong)
-    print('Random song name:', songs[randomSong])
-    # Load random song to the pygame mixer
-    try:
-        pygame.mixer.music.load(os.path.join(filepath, songs[randomSong]))
-    except Exception as Argument:
-        logging.exception("Error occurred while loading mp3 file")
-    
-    # Define the volume
-    pygame.mixer.music.set_volume(0.9)
-    
-    # Play the song
-    try:
-        pygame.mixer.music.play()
-    except Exception as Argument:
-        logging.exception("Error occurred while starting the song")
-    print("Music is now playing...")
-    pygame.mixer.music.set_endevent(END_OF_SONG)
-
-def startTheSong(song):
-    print('Selected Song number:', song)
-    print('Selected Song name:', songs[song])
-    # Load random song to the pygame mixer
-    try:
-        pygame.mixer.music.load(os.path.join(filepath, songs[song]))
-    except Exception as Argument:
-        logging.exception("Error occurred while loading mp3 file")
-    
-    # Define the volume
-    pygame.mixer.music.set_volume(0.9)
-    
-    # Start playing the song
-    try:
-        pygame.mixer.music.play()
-    except Exception as Argument:
-        logging.exception("Error occurred while starting the song")
-    print("Music is now playing...")
-    pygame.mixer.music.set_endevent(END_OF_SONG)
     
 def announceOFFMode():
     print('Announce OFF Mode')
@@ -170,9 +126,7 @@ def announceCarMode():
     pygame.mixer.music.set_volume(0.9)
     pygame.mixer.music.play()
 
-def stopTheMusic():
-    print('Stop Music or Sounds')
-    pygame.mixer.music.stop()
+
 
 def fadeOutTheLights():
     led_front_left.blink(0,1,0,1,1)
@@ -228,6 +182,20 @@ def setICLightsToIDLE():
     #led_red.value = 0.2
     led_ic.blink(1,0,1,1) # Let the IC LED pulse with 2 seconds period
 
+def activateLightsForMusic():
+    print("Music Mode for Lights started.")
+    # TODO: Move out to the THREAD, so that the Sleep Time will not affect the button behavior.
+    # Control LEDs
+    led_front_left.blink(0,0,1,1)
+    sleep(1)
+    led_front_right.blink(0,0,1,1)
+    sleep(0.5)
+    led_rear.blink(0,0,1,1)
+    sleep(0.5)
+    led_red.blink(0,0,1,1)
+    sleep(1)
+    led_blue.blink(0,0,1,1)
+
 def setIgnitionToOff():
     stopTheMusic()
     setICLightsToOff()
@@ -237,16 +205,87 @@ def setIgnitionToOff():
         setVehicleLightsToOff()
     print("Ignition State at setIgnitionOff", AMGBobbyCarIgnitionState)
     setICLightsToIDLE()
+
+def startMusicPlayer():
+    global MusicPlayerState
+    global randomSong
+    global previousSong
+    # Select random song from the list
+    randomSong = random.randrange(0,len(songs))
+    # Save the previous song before the next song selection
+    previousSong = randomSong
+    startTheSong(randomSong)
+    print('Previous Song:', previousSong)
+    # Turn on the LEDs for the music mode
+    activateLightsForMusic()
+    MusicPlayerState = 1
+    print("Music Player is ON")
+
+def startRandomSong():
+    # TODO: no song repetitions for at least 5 songs.
+    # Select random song from the list
+    randomSong = random.randrange(0,len(songs))
+    previousSong = randomSong
+    print('Random song number:', randomSong)
+    print('Random song name:', songs[randomSong])
+    # Load random song to the pygame mixer
+    try:
+        pygame.mixer.music.load(os.path.join(filepath, songs[randomSong]))
+    except Exception as Argument:
+        logging.exception("Error occurred while loading mp3 file")
     
+    # Define the volume
+    pygame.mixer.music.set_volume(0.9)
+    
+    # Play the song
+    try:
+        pygame.mixer.music.play()
+    except Exception as Argument:
+        logging.exception("Error occurred while starting the song")
+    print("Music is now playing...")
+    pygame.mixer.music.set_endevent(END_OF_SONG)
 
-#blink(on_time=1, off_time=1, n=None, background=True)
+def startTheSong(song):
+    print('Selected Song number:', song)
+    print('Selected Song name:', songs[song])
+    # Load random song to the pygame mixer
+    try:
+        pygame.mixer.music.load(os.path.join(filepath, songs[song]))
+    except Exception as Argument:
+        logging.exception("Error occurred while loading mp3 file")
+    
+    # Define the volume
+    pygame.mixer.music.set_volume(0.9)
+    
+    # Start playing the song
+    try:
+        pygame.mixer.music.play()
+    except Exception as Argument:
+        logging.exception("Error occurred while starting the song")
+    print("Music is now playing...")
+    pygame.mixer.music.set_endevent(END_OF_SONG)
+    
+def stopMusicPlayer():
+    global MusicPlayerState
+    stopTheMusic()
+    setICLightsToOff()
+    if led_rear.is_lit:
+        fadeOutTheLights()
+    else:
+        setVehicleLightsToOff()
+    MusicPlayerState = 0
+    print("Music Player is OFF")
 
-# Turn all LEDs off at startup.
-setVehicleLightsToOff()
-setICLightsToOff()
+def stopTheMusic():
+    print('Stop Music or Sounds')
+    pygame.mixer.music.stop()
 
-# Let the IC LEDs blink.
-setICLightsToIDLE()
+# How to use the LED BLINK function:
+# led_x.blink(on_time=1, off_time=1, n=None, background=True)
+
+########################
+# INIT BEHAVIOR
+########################
 
 # Initialize pygame library
 pygame.init()
@@ -266,21 +305,33 @@ songs = glob.glob(searchpath)
 # How many songs are available in the music folder
 print(len(songs), 'songs have been found.')
 
-previousSong = 0
+########################
+# STARTUP BEHAVIOR
+########################
 
-# Define the Vehicle Mode
-class VehicleMode(Enum):
-    OFF = 0
-    MUSIC = 1
-    CAR = 2
-AMGBobbyCarMode = 2
+# Turn all LEDs off at startup.
+setVehicleLightsToOff()
+setICLightsToOff()
 
-# Define the ignition state
-# 0 --> OFF
-# 1 --> ON
+# Let the IC LEDs blink.
+setICLightsToIDLE()
+
+
+#################################################
+# IMPORTANT STATE MACHINE VARIABLES
+#################################################
+
 AMGBobbyCarIgnitionState = 0
 
+MusicPlayerState = 0
+
 SireneState = 0
+
+previousSong = 0
+
+AMGBobbyCarMode = 2
+
+
 
 def turnICLEDOn():
     print("ON/OFF Switch is now ON")
@@ -297,84 +348,148 @@ IgnSwitch.when_pressed = startEngine
 IgnSwitch.when_released = stopEngine
 
 # Organize the time limit for Martinshorn
-sirenTimeLimit = 30 # in seconds
+sirenTimeLimit = 45 # in seconds
 martinshorn_start_time = time()
 
-
+# Blue Button shall only start or stop the music player
 def evaluateBlueButton():
     print("evaluateBlueButton")
-    global AMGBobbyCarIgnitionState
-    global AMGBobbyCarMode
-    print("Ignition State", AMGBobbyCarIgnitionState)
-    print("Car Mode", AMGBobbyCarMode)
-    if AMGBobbyCarIgnitionState == 0:
-        if AMGBobbyCarMode == 1:                
-            # Start music mode
-            startMusicMode()
-            # Select random song from the list
-            randomSong = random.randrange(0,len(songs))
-            # Save the previous song before the next song selection
-            previousSong = randomSong
-            startTheSong(randomSong)
-            print('Previous Song:', previousSong)
-            sleep(0.5)
-            led_red.blink(0,0,1,1)
-            sleep(1)
-            led_blue.blink(0,0,1,1)
-        elif AMGBobbyCarMode == 2:
-            startEngine()               
-            
-        AMGBobbyCarIgnitionState = 1
+    global MusicPlayerState
+    if MusicPlayerState == 0: # Player shall be started
+        startMusicPlayer()
     else:
-        print("Blue Button is pushed.Ignition is not equal to 0 and shall be set to 0.")
-        setIgnitionToOff()
-        AMGBobbyCarIgnitionState = 0
+        stopMusicPlayer()
     sleep(0.2)
 
-def evaluateSteeringWheelRightButton():
-    print('Right Button is pressed')
-    global AMGBobbyCarIgnitionState
-    global AMGBobbyCarMode
+def startNextSong():
     global previousSong
+    global randomSong
+    # Save the previous song before the next song selection
+    previousSong = randomSong
+    # Select random song from the list
+    randomSong = random.randrange(0,len(songs))
+    startTheSong(randomSong)
+    print('Previous Song:', previousSong)
+    sleep(0.5)
+
+def startMartinshorn():
     global SireneState
     global martinshorn_start_time
+    # Sound Control
+    try:
+        pygame.mixer.music.load(os.path.join(soundsPath, "martinshorn.mp3"))
+        pygame.mixer.music.set_volume(0.7)
+        pygame.mixer.music.play()
+        # reset the start point for the Shutdown timer
+        martinshorn_start_time = time() 
+        print("Martinshorn")
+    except Exception as Argument:
+        logging.exception("Error occurred while loading mp3 file")
+    
+    # Light Control
+    setVehicleLightsToPolizeiMode()
+    setICLightsToPolizeiMode()
+    
+    # State Control
+    SireneState = 1
+    print("SireneState: ", SireneState)
+
+def stopMartinshorn():
+    global SireneState
+    # Sound Control
+    stopTheMusic()
+    # Light Control
+    setICLightsToOff()
+    if led_rear.is_lit:
+        fadeOutTheLights()
+    else:
+        setVehicleLightsToOff()
+    #State Control
+    SireneState = 0
+    print("SireneState: ", SireneState)
+
+# Steering Wheel Right Button shall Start/Stop Martinshorn if the Engine is On/Off OR
+# it shall select the next song of the music player.
+# If the Engine is ON AND Music Player is running, then Music Player function has higher priority.
+def evaluateSteeringWheelRightButton():
+    print('Right Button is pressed')
+    global MusicPlayerState
+    global AMGBobbyCarIgnitionState
+    global SireneState
+    global martinshorn_start_time
+    
+    if MusicPlayerState == 1:
+        startNextSong()
+    elif MusicPlayerState == 0 and AMGBobbyCarIgnitionState == 1:
+        # Play the sirene sound.
+        if SireneState == 0:
+            startMartinshorn()            
+        else:
+            stopMartinshorn()
+    else:
+        print("Not wished situation")
+    
+    sleep(0.5)
+
+# Steering Wheel Left Button shall Start/Stop American Police Siren if the Engine is On/Off OR
+# it shall select the previous song of the music player.
+# If the Engine is ON AND Music Player is running, then Music Player function has higher priority.
+def evaluateSteeringWheelLeftButton():
+    print('Left Button is pressed')
+    global MusicPlayerState
+    global AMGBobbyCarIgnitionState
+    global SireneState
+    global martinshorn_start_time
+    
+    if MusicPlayerState == 1:
+        startNextSong()
+    elif MusicPlayerState == 0 and AMGBobbyCarIgnitionState == 1:
+        # Play the sirene sound.
+        if SireneState == 0:
+            startMartinshorn()            
+        else:
+            stopMartinshorn()
+    else:
+        print("Not wished situation")
+    
     if AMGBobbyCarMode == 1 and AMGBobbyCarIgnitionState == 1:
-        # Save the previous song before the next song selection
-        previousSong = randomSong
         # Select random song from the list
-        randomSong = random.randrange(0,len(songs))
-        startTheSong(randomSong)
+        # randomSong = random.randrange(0,len(songs))
+        startTheSong(previousSong)
         print('Previous Song:', previousSong)
-        sleep(0.5)
     elif AMGBobbyCarMode == 2:
         # Play the sirene sound.
         if SireneState == 0:
             try:
-                pygame.mixer.music.load(os.path.join(soundsPath, "martinshorn.mp3"))
-                pygame.mixer.music.set_volume(0.7)
+                pygame.mixer.music.load(os.path.join(soundsPath, "sirene_part1.mp3"))
+                pygame.mixer.music.set_volume(0.2)
                 pygame.mixer.music.play()
-                martinshorn_start_time = time() # reset the start point for the timer
-                print("Martinshorn")
+                print("Sirene")
             except Exception as Argument:
                 logging.exception("Error occurred while loading mp3 file")
-            setVehicleLightsToPolizeiMode()
-            setICLightsToPolizeiMode()
+            setVehicleLightsToPoliceMode()
+            setICLightsToPoliceMode()
             SireneState = 1
         else:
             setIgnitionToOff()
             SireneState = 0
-        sleep(0.5)
-
+    else:
+        print("Szenario noch nicht implementiert")
+    sleep(0.5)
+    
 def on_key_press(key):
     print(f"Key: {key}")
-    if key == Key.up:
+    if key == Key.shift: # Start/Stop the Music player
         evaluateBlueButton()
-    elif key == Key.right:
+    elif key == Key.page_up: # 
         evaluateSteeringWheelRightButton()
+    elif key == Key.page_down:
+        evaluateSteeringWheelLeftButton()
 
     
 def on_key_release(key):
-    print("Keyboard Key has been released")
+    if key == Key.enter: # Temporary solution, no need for now.
+        print("Keyboard Key has been released")
     
 try:
     listener_thread = threading.Thread(target=lambda: Listener(on_press=on_key_press, on_release=on_key_release).start())
@@ -382,88 +497,30 @@ try:
 except Exception as Argument:
         logging.exception("Error occurred while creating the thread")
 
+##########################
+# MAIN LOOP
+##########################
+
 while True:          
     
-    ###################
     # BLUE BUTTON LOGIC
-    ###################
     if blueButton.is_pressed:
         evaluateBlueButton()        
-    
-    ###################
-    # RED BUTTON LOGIC
-    ###################    
-    if redButton.is_pressed:
-        # Turn off the ignition (it doesn´t matter if it is On or Off)
-        print("Test Trigger for RED Button")
-        setIgnitionToOff()
         
-        # Imcrement the Vehicle Mode (Switch the mode of the AMG Bobby Car)
-        if AMGBobbyCarMode == 2:
-            AMGBobbyCarMode = 1
-        else:
-            AMGBobbyCarMode = AMGBobbyCarMode + 1
-        print("Red Button is pressed")
-        
-        if  AMGBobbyCarMode == 0:
-            if led_rear.is_lit:
-                fadeOutTheLights()
-            announceOFFMode()
-        elif AMGBobbyCarMode == 1:
-            if led_rear.is_lit:
-                fadeOutTheLights()
-            announceMusicMode()
-        elif AMGBobbyCarMode == 2:
-            if led_rear.is_lit:
-                fadeOutTheLights()
-            announceCarMode()
-        else:
-            print("Incorrect AMGBobbyCarMode")
-        # Prevent too frequent mode switching
-        sleep(1)
-    
-    ####################################
     # LEFT STEERING WHEEL BUTTON LOGIC #
-    ####################################
     if leftSteeringWheelButton.is_pressed:
-        print('Left Button is pressed')
-        if AMGBobbyCarMode == 1 and AMGBobbyCarIgnitionState == 1:
-            # Select random song from the list
-            # randomSong = random.randrange(0,len(songs))
-            startTheSong(previousSong)
-            print('Previous Song:', previousSong)
-        elif AMGBobbyCarMode == 2:
-            # Play the sirene sound.
-            if SireneState == 0:
-                try:
-                    pygame.mixer.music.load(os.path.join(soundsPath, "sirene_part1.mp3"))
-                    pygame.mixer.music.set_volume(0.2)
-                    pygame.mixer.music.play()
-                    print("Sirene")
-                except Exception as Argument:
-                    logging.exception("Error occurred while loading mp3 file")
-                setVehicleLightsToPoliceMode()
-                setICLightsToPoliceMode()
-                SireneState = 1
-            else:
-                setIgnitionToOff()
-                SireneState = 0
-        else:
-            print("Szenario noch nicht implementiert")
-        sleep(0.5)
+        evaluateSteeringWheelLeftButton()
     
-    #####################################
     # RIGHT STEERING WHEEL BUTTON LOGIC #
-    #####################################
     if rightSteeringWheelButton.is_pressed:
         evaluateSteeringWheelRightButton()
+    
     
     if SireneState == 1:
         elapsed_time = time() - martinshorn_start_time
         if elapsed_time >= sirenTimeLimit:
             print("Martinshorn will be automatically deactivated")
-            setIgnitionToOff()
-            SireneState = 0
+            stopMartinshorn()
             
     #rightSteeringWheelButton.when_released = stopTheMusic()    
     
@@ -471,8 +528,42 @@ while True:
     for event in pygame.event.get():
         if event.type == END_OF_SONG:
             print('End of song')
-            # Play the next random song only in music mode.
-            if AMGBobbyCarMode == 1 and AMGBobbyCarIgnitionState == 1:
+            # Play the next random song only if music player is on.
+            if MusicPlayerState == 1:
                 startRandomSong()
 
+###########################################################
+                #BACKUP
 
+
+    ###################
+    # RED BUTTON LOGIC
+    ###################    
+#     if redButton.is_pressed:
+#         # Turn off the ignition (it doesn´t matter if it is On or Off)
+#         print("Test Trigger for RED Button")
+#         setIgnitionToOff()
+#         
+#         # Imcrement the Vehicle Mode (Switch the mode of the AMG Bobby Car)
+#         if AMGBobbyCarMode == 2:
+#             AMGBobbyCarMode = 1
+#         else:
+#             AMGBobbyCarMode = AMGBobbyCarMode + 1
+#         print("Red Button is pressed")
+#         
+#         if  AMGBobbyCarMode == 0:
+#             if led_rear.is_lit:
+#                 fadeOutTheLights()
+#             announceOFFMode()
+#         elif AMGBobbyCarMode == 1:
+#             if led_rear.is_lit:
+#                 fadeOutTheLights()
+#             announceMusicMode()
+#         elif AMGBobbyCarMode == 2:
+#             if led_rear.is_lit:
+#                 fadeOutTheLights()
+#             announceCarMode()
+#         else:
+#             print("Incorrect AMGBobbyCarMode")
+#         # Prevent too frequent mode switching
+#         sleep(1)
